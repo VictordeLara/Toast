aeabd224e78927be1d140ecc8f2c390478c0dd79
VictordeLara <victorlara2010@gmail.com>
2022-03-14 02:11:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/toast/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void abrirToast(View view){
+
+        ImageView imagem = new ImageView( getApplicationContext());
+        imagem.setImageResource( android.R.drawable.star_big_on );
+
+        TextView textView = new TextView( getApplicationContext());
+        textView.setBackgroundResource(R.color.teal_200);
+        textView.setTextColor(R.color.purple_700);
+        textView.setText("Sup ?");
+
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView( textView );
+        toast.show();
+
+        //Toast.makeText(this, "Ação realizada com sucesso!", Toast.LENGTH_LONG).show();
+
+    }
+}
